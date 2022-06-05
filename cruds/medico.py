09f59302f8dc3854060eedb70c49634712da0217
Manoel Alves <manoelann@hotmail.com>
2022-06-05 05:25:50 -0300

    Adiciona Operação de Associação Medico-Hospital

--- cruds/medico.py
+++ cruds/medico.py
@@ -76,13 +76,102 @@ def confirma_dados(titulo:str, medico:Medico):
         else:
             valido = False
 
+def associa_medico_hospital():
+    titulo = 'MEDICO X HOSPITAL'
+    
+    comando = '''SELECT crm, nome FROM Medico'''
+    medicos = pega_info_db(comando)
+    
+    comando = '''SELECT cnpj, nome FROM Hospital'''
+    hospitais = pega_info_db(comando)
+    
+    qnt_medicos = len(medicos)
+    qnt_hospitais = len(hospitais)
+    
+    if qnt_medicos != 0 and qnt_hospitais != 0:
+        valido = True
+        alterna = 0
+        while True:
+            limpa_tela()
+            
+            imprime_titulo(titulo)
+            if alterna == 0:
+                print('Qual Medico?')
+                for i, medico in enumerate(medicos):
+                    print(f'{i + 1} - {medico[1]}')
+            else:
+                print('Trabalha em qual Hospital?')
+                for i, hospital in enumerate(hospitais):
+                    print(f'{i + 1} - {hospital[1]}')
+            imprime_linha()
+            print('0 - Voltar')
+            imprime_linha()
+            
+            if not valido:
+                mensagem_input_invalido('Opcao Invalida!')
+                valido = True
+            
+            if alterna == 0:
+                opcao = obter_opcao(qnt_medicos)
+            else:
+                opcao = obter_opcao(qnt_hospitais)
+            
+            if opcao == -1:
+                valido = False
+            else:
+                if alterna == 0:
+                    crm_medico = medicos[opcao - 1][0]
+                    alterna += 1
+                else:
+                    cnpj_hospital = hospitais[opcao - 1][0]
+                    break
+        
+        if opcao != 0:
+            print(crm_medico)
+            print(cnpj_hospital)
+            pausa()
+            comando = '''INSERT INTO Hospital_x_Medico (cnpj, crm) VALUES (:cnpj, :crm)'''
+            associa = altera_db(comando, {'cnpj':cnpj_hospital,'crm':crm_medico})
+            
+            if associa:
+                mensagem_sucesso(titulo, 'Medico', 'Associado')
+            else:
+                mensagem_erro(titulo, 'Medico', 'Associar')
+    else:
+        if qnt_hospitais == 0 and qnt_medicos == 0:
+            mensagem = 'Ainda não há Medicos e Hospitais Cadastrados!'
+        elif qnt_medicos == 0:
+            mensagem = 'Ainda não há Medicos Cadastrados!'
+        else:
+            mensagem = 'Ainda não há Hospitais Cadastrados!'
+        mensagem_query_vazia(titulo, mensagem)
+
 #------------------------------------------------------
 
 def altera_medico():
     pass
 
+#------------------------------------------------------
+
 def menu_relatorios_medico():
     pass
+
+def lista_medicos():
+    pass
+
+def lista_hospitais_medico():
+    pass
+
+def lista_enfermeiras_medico():
+    pass
+
+def lista_pacientes_medico():
+    pass
+
+def lista_telefones_medico():
+    pass
+
+#------------------------------------------------------
 
 def exclui_medico():
     titulo = 'EXCLUIR MEDICO'
@@ -117,6 +206,10 @@ def exclui_medico():
                 break
         
         if opcao != 0:
+            
+            comando = '''DELETE FROM Hospital_x_Medico WHERE crm=:crm'''
+            altera_db(comando, {'crm':crm_medico})
+            
             comando = '''DELETE FROM Medico WHERE crm=:crm'''
             excluido = altera_db(comando, {'crm':crm_medico})
             
--- utils/menu_cruds.py
+++ utils/menu_cruds.py
@@ -1,7 +1,7 @@
 from utils.geral import *
 from utils.obtencoes import obter_opcao
 from cruds.hospital import insere_hospital, altera_hospital, menu_relatorios_hospital, exclui_hospital
-from cruds.medico import insere_medico, altera_medico, menu_relatorios_medico, exclui_medico
+from cruds.medico import associa_medico_hospital, insere_medico, altera_medico, menu_relatorios_medico, exclui_medico
 from cruds.enfermeira import insere_enfermeira, altera_enfermeira, menu_relatorios_enfermeira, exclui_enfermeira
 from cruds.paciente import insere_paciente, altera_paciente, menu_relatorios_paciente, exclui_paciente
 
@@ -33,12 +33,21 @@ def crud_hospital():
             valido = False
 
 def crud_medico():
-    quant_opcoes = 4
+    quant_opcoes = 5
+    
     valido = True
     while True:
         limpa_tela()
         
-        imprime_menu_crud("Medico")
+        imprime_titulo(f'CRUD MEDICO')
+        print('1 - Inserir')
+        print('2 - Adiciona Hospital (trabalho)')
+        print('3 - Alterar')
+        print('4 - Relatorios')
+        print('5 - Excluir')
+        imprime_linha()
+        print('0 - Voltar')
+        imprime_linha()
         
         if not valido:
             mensagem_input_invalido('Opcao Invalida!')
@@ -49,10 +58,12 @@ def crud_medico():
         if opcao == 1:
             insere_medico()
         elif opcao == 2:
-            altera_medico()
+            associa_medico_hospital()
         elif opcao == 3:
-            menu_relatorios_medico()
+            altera_medico()
         elif opcao == 4:
+            menu_relatorios_medico()
+        elif opcao == 5:
             exclui_medico()
         elif opcao == 0:
             break
